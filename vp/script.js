0110fed897c77205f644e1f001f2114e96dc49f1
LawrenceParker <110305374+LawrenceParker@users.noreply.github.com>
2026-07-23 15:51:16 +0100

    Update script.js

--- vp/script.js
+++ vp/script.js
@@ -148,7 +148,7 @@ function normalizePlayers(rawRows){
     tournament: r.Tournament || 'Set 1',
     atk: parseInt(r.attRTG, 10) || 0,
     def_: parseInt(r.defRTG, 10) || 0,
-    ovr: parseInt(r.roleRTG, 10) || 0,
+    ovr: parseInt(r.ovrRTG, 10) || 0,
     roleIcon: r.RoleIcon || '',
     centreImage: r.CentreImage || ''
   })).filter(p => p.name && p.role);
